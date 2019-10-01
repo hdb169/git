d66d01a5b938e14d8948b2402caf64d373f10d00
hdb169 <hdb169@users.noreply.github.com>
2019-10-01 12:10:23 -0400

    Update short_file_to_do_1d_fit.C

--- 1D_2D_plots/short_file_to_do_1d_fit.C
+++ 1D_2D_plots/short_file_to_do_1d_fit.C
@@ -3,11 +3,11 @@
 #include <TH2.h>
 #include <TCanvas.h>
 #include <TObject.h>
-#define NRUNS 3
+#define NRUNS 2
 using namespace std;
 
-void test(){
-  Int_t runs[NRUNS] = {6359,6545,6524};//
+void hms_mean(){
+  Int_t runs[NRUNS] = {6359,6545};//,6524};//
 
   TFile *f[NRUNS];
   TTree *t[NRUNS];
@@ -16,9 +16,11 @@ void test(){
 
   TCanvas *c1[NRUNS];
   TCanvas *c2[NRUNS];
+  Double_t mean[NRUNS];
+  Double_t error[NRUNS];
 
   for(Int_t i=0; i<NRUNS; i++){
-    f[i]=new TFile(Form("coin_replay_production_%d_-1.root", runs[i]),"read");
+    f[i]=new TFile(Form("/lustre/expphy/volatile/hallc/spring17/hdbhatt/group/ROOTfiles/check_cal_calib_sep30/coin_replay_production_%d_-1.root", runs[i]),"read");
     t[i]=(TTree *)f[i]->Get("T");
 
     h1[i] = new TH1F(Form("h1[%d]", i), Form(""),100, 0.5, 1.5);
@@ -33,21 +35,28 @@ void test(){
 
 
 
-    gStyle->SetOptFit(1);
+    gStyle->SetOptFit(111111111);
     c1[i] = new TCanvas(Form("c%d]", i),"",1500,1200); 
     c1[i]->Divide(1,1); 
-c1[i]->cd(1);gPad->SetGridx();
+    c1[i]->cd(1);gPad->SetGridx();
     t[i]->Draw(Form("H.cal.etottracknorm>>h1[%d]", i ),"H.gtr.dp>-10&&H.gtr.dp<10 && H.gtr.beta>0.5 && H.gtr.beta< 1.5 && H.cer.npeSum>1.5");
     h1[i]->Fit("gaus","","",0.9, 1.1);
 
-    c1[i]->SaveAs(Form("sept30/hcal_%d.pdf",runs[i]));
+    mean[i] = h1[i]->GetFunction("gaus")->GetParameter(1);
+    error[i] = h1[i]->GetFunction("gaus")->GetParError(1);
+
+    // mean[i] = h1[i]->GetMean();
+    //error[i] = h1[i]->GetRMS();
 
 
-
-    
+ofstream txtfile ("hms_cal_mean.txt", ios::app);
+ txtfile<<runs[i]<< "   "<< "hcal_mean  ="<<mean[i]<< "   "<<"error = "<< error[i]<<endl;
+        c1[i]->SaveAs(Form("hms_mean_pdf/hcal_%d.pdf",runs[i]));
+ c1[i]->~TCanvas();//not to display the plots on canvas
     
   }
 
-  
+  //       c1[i]->SaveAs(Form("hms_mean_pdf/hcal_%d.pdf",runs[i]));
+ 
 
 }
