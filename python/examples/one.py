24d51da3e6ce21a483982e2a08f01fcc858954e8
hdb169 <hdb169@users.noreply.github.com>
2019-10-25 18:17:41 -0400

    Create one.py

---
+++ python/examples/one.py
@@ -0,0 +1,49 @@
+#2019 oct 25 Friday
+#this is the macro for the  very beginning concepts of python 
+print("basic stuffs in Python")
+print ("Hello, I am learning python")
+
+a =8
+b =8.7
+c = 9.09
+print (" a = " ,a) #to print a
+print (" a = " + str(a)) # u can use this too
+print (" b = " + str(b))
+print (" c = " + str(c))
+
+#name = 'hem'
+#print ('my name is {name}.')# this works only for python 3.6
+
+x = 1089
+print("The number is " + str(x))
+print("The number is %s" % x)
+print("The number is {0} and {0}".format(str(a))) # {zero}
+
+y =35
+print ("I am",y)#or
+print ("I am " + str(y)) #or
+print ("I am %s" %y) 
+
+#To print, the data type must be the same. 
+x = 10
+print(x+4)
+print(int(x)+4)
+print("x plus 4 = ",int(x) + 4)
+print(str(x) + " +" +  str(" what") + str(" will make 19"))# all are string type
+
+#Code #1 : Simple demonstration of format()
+# Python3 program to demonstarte the str.format() method 
+
+print("{}, The computer science is the Great choice.".format("Hi Mr. Bhatt"))
+
+print("{}, The computer science is the Great choice.".format(str("Hi Mr. Bhatt"))) # you dont need str here
+
+apple = "This is Bidhan and Sakshi's {}" 
+print(apple.format("Dad"))
+
+print ("This is Biddan and Sakshi's {}.".format("Dad"))
+
+bachha = "my daughter is {} years old"
+print(bachha.format(8))
+
+#Syntax : { } { } .format(value1, value2)
