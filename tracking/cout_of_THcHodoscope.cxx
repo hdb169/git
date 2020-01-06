7b2d4ceebbdaadf93d2bac9f6d1677e7d3b4c617
hdb169 <hdb169@users.noreply.github.com>
2020-01-06 16:36:02 -0500

    Create cout_of_THcHodoscope.cxx

---
+++ tracking/cout_of_THcHodoscope.cxx
@@ -0,0 +1,356 @@
+
+//run 6263
+hcana_event 100001
+
+hcana_event 100001
+ spec = P
+ lowtemp = -30
+ hitemp = 30
+ lowtemp = -22.5
+ hitemp = 22.5
+ lowtemp = -42.75
+ hitemp = 42.75
+ lowtemp = -30
+ hitemp = 30
+ ip = 1  sum_good_track_test = 0
+ ip = 2  sum_good_track_test = 0
+ ip = 3  sum_good_track_test = 0
+ ip = 4  sum_good_track_test = 0
+ number of planes hits = 0
+ fTrackEffTestNScinPlanes = 4
+ GoodScinHits = 0
+ ************
+ spec = H
+ lowtemp = -35.05
+ hitemp = 32.45
+ lowtemp = -12.55
+ hitemp = 9.95
+ lowtemp = -34.35
+ hitemp = 33.15
+ lowtemp = -13.65
+ hitemp = 8.85
+ New clus pl = 1 hit = 0 pad = 6 clus = 1 cl size = 1 pos -20.05
+ New clus pl = 2 hit = 0 pad = 6 clus = 1 cl size = 1 pos -5.05
+ New clus pl = 3 hit = 0 pad = 6 clus = 1 cl size = 1 pos -19.35
+ New clus pl = 4 hit = 0 pad = 6 clus = 1 cl size = 1 pos -6.15
+no two good times  plane = 4 hit = 1
+plane = 1 Cluster = 1 size = 1 pos = -20.05 inside = 1 lo = -35.05 hi = 32.45
+plane = 2 Cluster = 1 size = 1 pos = -5.05 inside = 1 lo = -12.55 hi = 9.95
+plane = 3 Cluster = 1 size = 1 pos = -19.35 inside = 1 lo = -34.35 hi = 33.15
+plane = 4 Cluster = 1 size = 1 pos = -6.15 inside = 1 lo = -13.65 hi = 8.85
+ ip 1 clus = 0 good for track = 1
+ ip = 1  sum_good_track_test = 1
+ ip 2 clus = 0 good for track = 1
+ ip = 2  sum_good_track_test = 1
+ ip 3 clus = 0 good for track = 1
+ ip = 3  sum_good_track_test = 1
+ ip 4 clus = 0 good for track = 1
+ ip = 4  sum_good_track_test = 1
+ number of planes hits = 4
+ fTrackEffTestNScinPlanes = 4
+ 4 good planes  xdiff = 1 ydiff = 1
+ GoodScinHits = 1
+ ************
+
+hcana_event 100002
+
+hcana_event 100002
+ spec = P
+ lowtemp = -30
+ hitemp = 30
+ lowtemp = -22.5
+ hitemp = 22.5
+ lowtemp = -42.75
+ hitemp = 42.75
+ lowtemp = -30
+ hitemp = 30
+ New clus pl = 1 hit = 0 pad = 6 clus = 1 cl size = 1 pos -7.5
+ New clus pl = 2 hit = 0 pad = 6 clus = 1 cl size = 1 pos 7.5
+Add to cluster  pl = 2 hit = 1 pad = 7 clus =  1 cl size = 2 pos 0
+ New clus pl = 3 hit = 0 pad = 6 clus = 1 cl size = 1 pos -14.25
+ New clus pl = 4 hit = 0 pad = 10 clus = 1 cl size = 1 pos 5
+plane = 1 Cluster = 1 size = 1 pos = -7.5 inside = 1 lo = -30 hi = 30
+plane = 2 Cluster = 1 size = 2 pos = 3.75 inside = 1 lo = -22.5 hi = 22.5
+plane = 3 Cluster = 1 size = 1 pos = -14.25 inside = 1 lo = -42.75 hi = 42.75
+plane = 4 Cluster = 1 size = 1 pos = 5 inside = 1 lo = -30 hi = 30
+ ip 1 clus = 0 good for track = 1
+ ip = 1  sum_good_track_test = 1
+ ip 2 clus = 0 good for track = 1
+ ip = 2  sum_good_track_test = 1
+ ip 3 clus = 0 good for track = 1
+ ip = 3  sum_good_track_test = 1
+ ip 4 clus = 0 good for track = 1
+ ip = 4  sum_good_track_test = 1
+ number of planes hits = 4
+ fTrackEffTestNScinPlanes = 4
+ 4 good planes  xdiff = 1 ydiff = 1
+ GoodScinHits = 1
+ ************
+ spec = H
+ lowtemp = -35.05
+ hitemp = 32.45
+ lowtemp = -12.55
+ hitemp = 9.95
+ lowtemp = -34.35
+ hitemp = 33.15
+ lowtemp = -13.65
+ hitemp = 8.85
+
+ New cluster plane = 1 hit = 0 pad = 2 clus = 1 cl size = 1 positive offset of the plane  -50.05
+ New cluster plane = 1 hit = 1 pad = 8 clus = 2 cl size = 1 positive offset of the plane  -5.05
+ Add to cluster  plane = 1 hit = 2 pad = 9 clus =  2 cl size = 2 positive offset of the plane  2.45
+ Add to cluster  plane = 1 hit = 3 pad = 10 clus =  2 cl size = 3 positive offset of the plane  9.95
+ New cluster plane = 2 hit = 0 pad = 5 clus = 1 cl size = 1 positive offset of the plane  2.45
+ New cluster plane = 2 hit = 1 pad = 8 clus = 2 cl size = 1 positive offset of the plane  -20.05
+
+ no two good times  plane = 2 hit = 2
+ no two good times  plane = 3 hit = 0
+ New cluster plane = 3 hit = 1 pad = 14 clus = 1 cl size = 1 positive offset of the plane  40.65
+ no two good times  plane = 4 hit = 0
+ no two good times  plane = 4 hit = 1
+ New cluster plane = 4 hit = 2 pad = 9 clus = 1 cl size = 1 positive offset of the plane  -28.65
+ no two good times  plane = 4 hit = 3
+
+ plane = 1 Cluster = 1 size = 1 positive offset of the plane  = -50.05 inside = 0 lo = -35.05 hi = 32.45
+ plane = 1 Cluster = 2 size = 3 positive offset of the plane  = 2.45 inside = 1 lo = -35.05 hi = 32.45
+ plane = 2 Cluster = 1 size = 1 positive offset of the plane  = 2.45 inside = 1 lo = -12.55 hi = 9.95
+ plane = 2 Cluster = 2 size = 1 positive offset of the plane  = -20.05 inside = 0 lo = -12.55 hi = 9.95
+ plane = 3 Cluster = 1 size = 1 positive offset of the plane  = 40.65 inside = 0 lo = -34.35 hi = 33.15
+ plane = 4 Cluster = 1 size = 1 positive offset of the plane  = -28.65 inside = 0 lo = -13.65 hi = 8.85
+
+ ip 1 clus = 0 good for track = 0
+ ip 1 clus = 1 good for track = 1
+ ip = 1  sum_good_track_test = 1
+
+ ip 2 clus = 0 good for track = 1
+ ip 2 clus = 1 good for track = 0
+ ip = 2  sum_good_track_test = 1
+
+ ip 3 clus = 0 good for track = 0
+ ip = 3  sum_good_track_test = 0
+
+ ip 4 clus = 0 good for track = 0
+ ip = 4  sum_good_track_test = 0
+
+ number of planes hits = 2
+ fTrackEffTestNScinPlanes = 4
+ GoodScinHits = 0
+ ************
+
+hcana_event 100003
+
+hcana_event 100003
+ spec = P
+ lowtemp = -30
+ hitemp = 30
+ lowtemp = -22.5
+ hitemp = 22.5
+ lowtemp = -42.75
+ hitemp = 42.75
+ lowtemp = -30
+ hitemp = 30
+no two good times  plane = 1 hit = 0
+no two good times  plane = 4 hit = 0
+no two good times  plane = 4 hit = 1
+ ip = 1  sum_good_track_test = 0
+ ip = 2  sum_good_track_test = 0
+ ip = 3  sum_good_track_test = 0
+ ip = 4  sum_good_track_test = 0
+ number of planes hits = 0
+ fTrackEffTestNScinPlanes = 4
+ GoodScinHits = 0
+ ************
+ spec = H
+ lowtemp = -35.05
+ hitemp = 32.45
+ lowtemp = -12.55
+ hitemp = 9.95
+ lowtemp = -34.35
+ hitemp = 33.15
+ lowtemp = -13.65
+ hitemp = 8.85
+ New cluster plane = 2 hit = 0 pad = 7 clus = 1 cl size = 1 positive offset of the plane  -12.55
+ New cluster plane = 3 hit = 0 pad = 12 clus = 1 cl size = 1 positive offset of the plane  25.65
+ New cluster plane = 4 hit = 0 pad = 6 clus = 1 cl size = 1 positive center -6.15
+plane = 2 Cluster = 1 size = 1 positive center = -12.55 inside = 1 lo = -12.55 hi = 9.95
+plane = 3 Cluster = 1 size = 1 positive center = 25.65 inside = 1 lo = -34.35 hi = 33.15
+plane = 4 Cluster = 1 size = 1 positive center = -6.15 inside = 1 lo = -13.65 hi = 8.85
+ ip = 1  sum_good_track_test = 0
+ ip 2 clus = 0 good for track = 1
+ ip = 2  sum_good_track_test = 1
+ ip 3 clus = 0 good for track = 1
+ ip = 3  sum_good_track_test = 1
+ ip 4 clus = 0 good for track = 1
+ ip = 4  sum_good_track_test = 1
+ number of planes hits = 3
+ fTrackEffTestNScinPlanes = 4
+ GoodScinHits = 0
+ ************
+
+hcana_event 100004
+
+hcana_event 100004
+ spec = P
+ lowtemp = -30
+ hitemp = 30
+ lowtemp = -22.5
+ hitemp = 22.5
+ lowtemp = -42.75
+ hitemp = 42.75
+ lowtemp = -30
+ hitemp = 30
+ New cluster plane = 1 hit = 0 pad = 6 clus = 1 cl size = 1 positive center -7.5
+ New cluster plane = 2 hit = 0 pad = 8 clus = 1 cl size = 1 positive center -7.5
+Add to cluster  plane = 2 hit = 1 pad = 9 clus =  1 cl size = 2 positive center -15
+ New cluster plane = 3 hit = 0 pad = 5 clus = 1 cl size = 1 positive center -23.75
+Add to cluster  plane = 3 hit = 1 pad = 6 clus =  1 cl size = 2 positive center -14.25
+ New cluster plane = 4 hit = 0 pad = 14 clus = 1 cl size = 1 positive center -15
+plane = 1 Cluster = 1 size = 1 positive center = -7.5 inside = 1 lo = -30 hi = 30
+plane = 2 Cluster = 1 size = 2 positive center = -11.25 inside = 1 lo = -22.5 hi = 22.5
+plane = 3 Cluster = 1 size = 2 positive center = -19 inside = 1 lo = -42.75 hi = 42.75
+plane = 4 Cluster = 1 size = 1 positive center = -15 inside = 1 lo = -30 hi = 30
+ ip 1 clus = 0 good for track = 1
+ ip = 1  sum_good_track_test = 1
+ ip 2 clus = 0 good for track = 1
+ ip = 2  sum_good_track_test = 1
+ ip 3 clus = 0 good for track = 1
+ ip = 3  sum_good_track_test = 1
+ ip 4 clus = 0 good for track = 1
+ ip = 4  sum_good_track_test = 1
+ number of planes hits = 4
+ fTrackEffTestNScinPlanes = 4
+ 4 good planes  xdiff = 1 ydiff = 1
+ GoodScinHits = 1
+ ************
+ spec = H
+ lowtemp = -35.05
+ hitemp = 32.45
+ lowtemp = -12.55
+ hitemp = 9.95
+ lowtemp = -34.35
+ hitemp = 33.15
+ lowtemp = -13.65
+ hitemp = 8.85
+ New cluster plane = 1 hit = 0 pad = 1 clus = 1 cl size = 1 positive center -57.55
+Add to cluster  plane = 1 hit = 1 pad = 2 clus =  1 cl size = 2 positive center -50.05
+ New cluster plane = 1 hit = 2 pad = 14 clus = 2 cl size = 1 positive center 39.95
+no two good times  plane = 2 hit = 0
+no two good times  plane = 2 hit = 1
+ New cluster plane = 2 hit = 2 pad = 8 clus = 1 cl size = 1 positive center -20.05
+Add to cluster  plane = 2 hit = 3 pad = 9 clus =  1 cl size = 2 positive center -27.55
+Add to cluster  plane = 2 hit = 4 pad = 10 clus =  1 cl size = 3 positive center -35.05
+ New cluster plane = 3 hit = 0 pad = 1 clus = 1 cl size = 1 positive center -56.85
+no two good times  plane = 4 hit = 0
+ New cluster plane = 4 hit = 1 pad = 7 clus = 1 cl size = 1 positive center -13.65
+ New cluster plane = 4 hit = 2 pad = 9 clus = 2 cl size = 1 positive center -28.65
+no two good times  plane = 4 hit = 3
+plane = 1 Cluster = 1 size = 2 positive center = -53.8 inside = 0 lo = -35.05 hi = 32.45
+plane = 1 Cluster = 2 size = 1 positive center = 39.95 inside = 0 lo = -35.05 hi = 32.45
+plane = 2 Cluster = 1 size = 3 positive center = -27.55 inside = 0 lo = -12.55 hi = 9.95
+plane = 3 Cluster = 1 size = 1 positive center = -56.85 inside = 0 lo = -34.35 hi = 33.15
+plane = 4 Cluster = 1 size = 1 positive center = -13.65 inside = 1 lo = -13.65 hi = 8.85
+plane = 4 Cluster = 2 size = 1 positive center = -28.65 inside = 0 lo = -13.65 hi = 8.85
+ ip 1 clus = 0 good for track = 0
+ ip 1 clus = 1 good for track = 0
+ ip = 1  sum_good_track_test = 0
+ ip 2 clus = 0 good for track = 0
+ ip = 2  sum_good_track_test = 0
+ ip 3 clus = 0 good for track = 0
+ ip = 3  sum_good_track_test = 0
+ ip 4 clus = 0 good for track = 1
+ ip 4 clus = 1 good for track = 0
+ ip = 4  sum_good_track_test = 1
+ number of planes hits = 1
+ fTrackEffTestNScinPlanes = 4
+ GoodScinHits = 0
+ ************
+
+
+
+hcana_event 100008
+
+hcana_event 100008
+ spec = P
+ lowtemp = -30
+ hitemp = 30
+ lowtemp = -22.5
+ hitemp = 22.5
+ lowtemp = -42.75
+ hitemp = 42.75
+ lowtemp = -30
+ hitemp = 30
+ New cluster plane = 1 hit = 0 pad = 11 clus = 1 cl size = 1 positive center 30
+ New cluster plane = 2 hit = 0 pad = 8 clus = 1 cl size = 1 positive center -7.5
+ New cluster plane = 3 hit = 0 pad = 12 clus = 1 cl size = 1 positive center 42.75
+ New cluster plane = 4 hit = 0 pad = 13 clus = 1 cl size = 1 positive center -10
+plane = 1 Cluster = 1 size = 1 positive center = 30 inside = 1 lo = -30 hi = 30
+plane = 2 Cluster = 1 size = 1 positive center = -7.5 inside = 1 lo = -22.5 hi = 22.5
+plane = 3 Cluster = 1 size = 1 positive center = 42.75 inside = 1 lo = -42.75 hi = 42.75
+plane = 4 Cluster = 1 size = 1 positive center = -10 inside = 1 lo = -30 hi = 30
+ ip 1 clus = 0 good for track = 1
+ ip = 1  sum_good_track_test = 1
+ ip 2 clus = 0 good for track = 1
+ ip = 2  sum_good_track_test = 1
+ ip 3 clus = 0 good for track = 1
+ ip = 3  sum_good_track_test = 1
+ ip 4 clus = 0 good for track = 1
+ ip = 4  sum_good_track_test = 1
+ number of planes hits = 4
+ fTrackEffTestNScinPlanes = 4
+ 4 good planes  xdiff = 1 ydiff = 1
+ GoodScinHits = 1
+ ************
+ spec = H
+ lowtemp = -35.05
+ hitemp = 32.45
+ lowtemp = -12.55
+ hitemp = 9.95
+ lowtemp = -34.35
+ hitemp = 33.15
+ lowtemp = -13.65
+ hitemp = 8.85
+no two good times  plane = 1 hit = 0
+ New cluster plane = 1 hit = 1 pad = 2 clus = 1 cl size = 1 positive center -50.05
+Add to cluster  plane = 1 hit = 2 pad = 3 clus =  1 cl size = 2 positive center -42.55
+Add to cluster  plane = 1 hit = 3 pad = 4 clus =  1 cl size = 3 positive center -35.05
+Add to cluster  plane = 1 hit = 4 pad = 5 clus =  1 cl size = 4 positive center -27.55
+ New cluster plane = 1 hit = 5 pad = 10 clus = 2 cl size = 1 positive center 9.95
+Add to cluster  plane = 1 hit = 6 pad = 11 clus =  2 cl size = 2 positive center 17.45
+Add to cluster  plane = 1 hit = 7 pad = 12 clus =  2 cl size = 3 positive center 24.95
+no two good times  plane = 2 hit = 0
+no two good times  plane = 2 hit = 1
+ New cluster plane = 2 hit = 2 pad = 3 clus = 1 cl size = 1 positive center 17.45
+Add to cluster  plane = 2 hit = 3 pad = 4 clus =  1 cl size = 2 positive center 9.95
+Add to cluster  plane = 2 hit = 4 pad = 5 clus =  1 cl size = 3 positive center 2.45
+ New cluster plane = 3 hit = 0 pad = 2 clus = 1 cl size = 1 positive center -49.35
+no two good times  plane = 3 hit = 1
+ New cluster plane = 3 hit = 2 pad = 16 clus = 2 cl size = 1 positive center 55.65
+ New cluster plane = 4 hit = 0 pad = 3 clus = 1 cl size = 1 positive center 16.35
+Add to cluster  plane = 4 hit = 1 pad = 4 clus =  1 cl size = 2 positive center 8.85
+ New cluster plane = 4 hit = 2 pad = 6 clus = 2 cl size = 1 positive center -6.15
+no two good times  plane = 4 hit = 3
+ New cluster plane = 4 hit = 4 pad = 8 clus = 3 cl size = 1 positive center -21.15
+plane = 1 Cluster = 1 size = 4 positive center = -38.8 inside = 0 lo = -35.05 hi = 32.45
+plane = 1 Cluster = 2 size = 3 positive center = 17.45 inside = 1 lo = -35.05 hi = 32.45
+plane = 2 Cluster = 1 size = 3 positive center = 9.95 inside = 1 lo = -12.55 hi = 9.95
+plane = 3 Cluster = 1 size = 1 positive center = -49.35 inside = 0 lo = -34.35 hi = 33.15
+plane = 3 Cluster = 2 size = 1 positive center = 55.65 inside = 0 lo = -34.35 hi = 33.15
+plane = 4 Cluster = 1 size = 2 positive center = 12.6 inside = 0 lo = -13.65 hi = 8.85
+plane = 4 Cluster = 2 size = 1 positive center = -6.15 inside = 1 lo = -13.65 hi = 8.85
+plane = 4 Cluster = 3 size = 1 positive center = -21.15 inside = 0 lo = -13.65 hi = 8.85
+ ip 1 clus = 0 good for track = 0
+ ip 1 clus = 1 good for track = 1
+ ip = 1  sum_good_track_test = 1
+ ip 2 clus = 0 good for track = 1
+ ip = 2  sum_good_track_test = 1
+ ip 3 clus = 0 good for track = 0
+ ip 3 clus = 1 good for track = 0
+ ip = 3  sum_good_track_test = 0
+ ip 4 clus = 0 good for track = 0
+ ip 4 clus = 1 good for track = 1
+ ip 4 clus = 2 good for track = 0
+ ip = 4  sum_good_track_test = 1
+ number of planes hits = 3
+ fTrackEffTestNScinPlanes = 4
+ GoodScinHits = 0
+ ************
