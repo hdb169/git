633555d687fd6ab7a23c50c1a77cc50647fc6b0e
hdb169 <hdb169@users.noreply.github.com>
2020-01-20 15:19:02 -0500

    Update data_acc_substract.C

--- yield/data_acc_substract.C
+++ yield/data_acc_substract.C
@@ -76,7 +76,7 @@ Double_t hiq = 10;
 
 //this macro reads a few variables from the rootfile and just takes the counts by doing the accidental subtraction which is calculated by using the cointime plot. we will have cut 1 i.e all the cuts execept the accidental subtraction cut and the other is cut 2 with all the cuts plus the accidental cut.
 #include "header.h"
-void data_acc_substract(Int_t runNum){
+void data_acc(Int_t runNum){//6263 data, 6261 dummy for trial
   gStyle->SetOptStat(0);
 #include "gStyle.h" 
 #include "bin.h"
@@ -93,22 +93,22 @@ void data_acc_substract(Int_t runNum){
   Double_t pionMass = 0.1395701835;   
   Double_t pbeta,hbeta,pdelta,hdelta,hcaletot,pcaletottrack,pcalepr,hcalepr,paeronpe,hcernpe,phg,W,Q2,xbj,ctime,Epi,Epi_d,Epi_acc,z,z_d,z_acc,pkinomega,PgtrP,HgtrP,hhodstarttime,phodstarttime,hdipole, pdipole,pcaletotnorm,goodscinhit,betanotrack,pdcntrack,pdcch1nhit,pdcch2nhit;
  
-  TH1D *h_ctime = new TH1D("h_ctime", "h_ctime (ns)", binct, loct, hict);
-  TH1D *h_ctime_d = new TH1D("h_ctime_d", "h_ctime_d (ns)", binct, loct, hict);
-  TH1D *h_ctime_acc = new TH1D("h_ctime_acc", "h_ctime_acc", binct, loct, hict);
+  TH1D *h_datactime = new TH1D("h_datactime", "h_datactime (ns)", binct, loct, hict);
+  TH1D *h_datactime_d = new TH1D("h_datactime_d", "h_datactime_d (ns)", binct, loct, hict);
+  TH1D *h_datactime_acc = new TH1D("h_datactime_acc", "h_datactime_acc", binct, loct, hict);
 
 
-  TH1D *h_z = new TH1D("h_z","h_z", binz, loz, hiz);        
-  TH1D *h_z_d = new TH1D("h_z_d","h_z_d", binz, loz, hiz);                                      
-  TH1D *h_z_acc = new TH1D("h_z_acc","h_z_acc", binz, loz, hiz);                                      
+  TH1D *h_dataz = new TH1D("h_dataz","h_dataz", binz, loz, hiz);        
+  TH1D *h_dataz_d = new TH1D("h_dataz_d","h_dataz_d", binz, loz, hiz);                                      
+  TH1D *h_dataz_acc = new TH1D("h_dataz_acc","h_dataz_acc", binz, loz, hiz);                                      
 
                               
-  TH1D *h_x = new TH1D("h_x","h_x", binx, lox, hix);   
-  TH1D *h_x_d = new TH1D("h_x_d","h_x_d", binx, lox, hix);   
-  TH1D *h_x_acc = new TH1D("h_x_acc","h_x_acc", binx, lox, hix);                                      
+  TH1D *h_datax = new TH1D("h_datax","h_datax", binx, lox, hix);   
+  TH1D *h_datax_d = new TH1D("h_datax_d","h_datax_d", binx, lox, hix);   
+  TH1D *h_datax_acc = new TH1D("h_datax_acc","h_datax_acc", binx, lox, hix);                                      
 
                                  
-  TH1D *h_Q2d = new TH1D("h_Q2d","h_Q2d",binq, loq, hiq);  
+  TH1D *h_dataQ2d = new TH1D("h_dataQ2d","h_dataQ2d",binq, loq, hiq);  
 
 
 
@@ -131,16 +131,16 @@ void data_acc_substract(Int_t runNum){
 
       if (kk % 700000 == 0) cout << kk*100/nentriesD << "   % of data done" << endl;
 
-      data_cut = paeronpe>2.0 && pcalepr< 0.1 && pcalepr > 0.00001 && pcaletottrack < 0.4 && pcaletottrack> 0.1 && hcaletot > 0.8 && hcaletot < 1.2 && hdelta> -10 && hdelta < 10 && pdelta >-10 && pdelta < 20  && hcernpe > 1.0 &&  pbeta > 0.8 && pbeta < 1.2 &&hbeta > 0.8 &&hbeta < 1.2 && phg>2 && hhodstarttime ==1 &&phodstarttime == 1 && hdipole==1 && pdipole ==1;
+      data_cut = paeronpe>2.0 && pcalepr< 0.1 && pcalepr > 0.00001 && pcaletottrack < 0.4 && pcaletottrack> 0.1 && hcaletot > 0.8 && hcaletot < 1.2 && hdelta> -10 && hdelta < 10 && pdelta >-10 && pdelta < 20  && hcernpe > 1.5 &&  pbeta > 0.7 && pbeta < 1.3 &&hbeta > 0.7 &&hbeta < 1.3  && hhodstarttime ==1 &&phodstarttime == 1 && hdipole==1 && pdipole ==1;//no hgcer cut 
   
       if(data_cut)
 	{    cntsepi++;
 
-	  h_ctime->Fill(ctime);
+	  h_datactime->Fill(ctime);
 	  Epi = sqrt(pow(pionMass,2) + pow(PgtrP,2));
 	  z = Epi/pkinomega;
-	  h_z->Fill(z);
-          h_x->Fill(xbj);
+	  h_dataz->Fill(z);
+          h_datax->Fill(xbj);
 
 	}
     }
@@ -151,23 +151,23 @@ void data_acc_substract(Int_t runNum){
 
   cout<<"calulating the max peak value of the ctime dist"<<endl;
 
-  Int_t bin_max = h_ctime->GetMaximumBin();
-  Double_t max_value = h_ctime->GetBinCenter(bin_max);
+  Int_t bin_max = h_datactime->GetMaximumBin();
+  Double_t max_value = h_datactime->GetBinCenter(bin_max);
   cout<<"ctime peak is at = "<< max_value <<endl;
 
   for (int ll=0; ll<nentriesD;  ll++){
     tt->GetEntry(ll);
     if (ll % 700000 == 0) cout << ll*100/nentriesD << "   % of data done" << endl;
 
-    pid_cut = paeronpe>2.0 && pcalepr< 0.1 && pcalepr > 0.00001 && pcaletottrack < 0.4 && pcaletottrack> 0.1 && hcaletot > 0.8 && hcaletot < 1.2 && hdelta> -10 && hdelta < 10 && pdelta >-10 && pdelta < 20  && hcernpe > 1.0 &&  pbeta > 0.8 && pbeta < 1.2 &&hbeta > 0.8 &&hbeta < 1.2 && phg>2 && hhodstarttime ==1 &&phodstarttime == 1 && hdipole==1 && pdipole ==1 && ctime > (max_value-2) && ctime < (max_value+2);
+    pid_cut =  paeronpe>2.0 && pcalepr< 0.1 && pcalepr > 0.00001 && pcaletottrack < 0.4 && pcaletottrack> 0.1 && hcaletot > 0.8 && hcaletot < 1.2 && hdelta> -10 && hdelta < 10 && pdelta >-10 && pdelta < 20  && hcernpe > 1.5 &&  pbeta > 0.7 && pbeta < 1.3 &&hbeta > 0.7 &&hbeta < 1.3  && hhodstarttime ==1 &&phodstarttime == 1 && hdipole==1 && pdipole ==1 && ctime > (max_value-2) && ctime < (max_value+2);
   
     if(pid_cut){
       evts++;
-      h_ctime_d->Fill(ctime);
+      h_datactime_d->Fill(ctime);
       Epi_d = sqrt(pow(pionMass,2) + pow(PgtrP,2));
       z_d = Epi_d/pkinomega;
-      h_z_d->Fill(z_d);
-      h_x_d->Fill(xbj);
+      h_dataz_d->Fill(z_d);
+      h_datax_d->Fill(xbj);
 
 
     }
@@ -186,19 +186,19 @@ void data_acc_substract(Int_t runNum){
       if (mm % 700000 == 0) cout << mm*100/nentriesD << "   % of data done" << endl;
 
 
-      pid_cut_acc = paeronpe>2.0 && pcalepr< 0.1 && pcalepr > 0.00001 && pcaletottrack < 0.4 && pcaletottrack> 0.1 && hcaletot > 0.8 && hcaletot < 1.2 && hdelta> -10 && hdelta < 10 && pdelta >-10 && pdelta < 20  && hcernpe > 1.0 &&  pbeta > 0.8 && pbeta < 1.2 &&hbeta > 0.8 &&hbeta < 1.2 && phg>2 && hhodstarttime ==1 &&phodstarttime == 1 && hdipole==1 && pdipole ==1 ;
+      pid_cut_acc =   paeronpe>2.0 && pcalepr< 0.1 && pcalepr > 0.00001 && pcaletottrack < 0.4 && pcaletottrack> 0.1 && hcaletot > 0.8 && hcaletot < 1.2 && hdelta> -10 && hdelta < 10 && pdelta >-10 && pdelta < 20  && hcernpe > 1.5 &&  pbeta > 0.7 && pbeta < 1.3 &&hbeta > 0.7 &&hbeta < 1.3  && hhodstarttime ==1 &&phodstarttime == 1 && hdipole==1 && pdipole ==1;
 
-      accR_cut        =   ctime > (max_value-14) && ctime < (max_value-6);
-      accL_cut        =   ctime > (max_value+6) && ctime < (max_value+14);
+      accR_cut        =   ctime > (max_value-18) && ctime < (max_value-6);
+      accL_cut        =   ctime > (max_value+6) && ctime < (max_value+18);
      
       acc_cut    = (accL_cut || accR_cut) && pid_cut_acc;
       if(acc_cut){
 	counts++;
-	h_ctime_acc->Fill(ctime);
+	h_datactime_acc->Fill(ctime);
 	Epi_acc = sqrt(pow(pionMass,2) + pow(PgtrP,2));
 	z_acc = Epi_acc/pkinomega;
-	h_z_acc->Fill(z_acc);
-	h_x_acc->Fill(xbj);
+	h_dataz_acc->Fill(z_acc);
+	h_datax_acc->Fill(xbj);
 
 
 
@@ -210,11 +210,11 @@ void data_acc_substract(Int_t runNum){
 
   
   // ofstream printout("txtfile/acc_sub.txt",ios::app);
-  //printout<<"counts with other than ctime cut="<< h_ctime->Integral()<< " counts should ="<< h_ctime_should->Integral()<<" eff = "<<h_ctime_did->Integral()/h_ctime_should->Integral()<<endl;
+  //printout<<"counts with other than ctime cut="<< h_datactime->Integral()<< " counts should ="<< h_datactime_should->Integral()<<" eff = "<<h_datactime_did->Integral()/h_datactime_should->Integral()<<endl;
   cout<<"total counts = "<<nentriesD<<"  "<<endl;
-  cout<<"counts other than ctime cut = "<<h_ctime->Integral()<<endl;
-  cout<<"counts with ctime cut = "<<h_ctime_d->Integral()<<endl;
-  cout<<"counts in accidentals with = "<<h_ctime_acc->Integral()/4.0<<endl;
+  cout<<"counts other than ctime cut = "<<h_datactime->Integral()<<endl;
+  cout<<"counts with ctime cut = "<<h_datactime_d->Integral()<<endl;
+  cout<<"counts in accidentals with = "<<h_datactime_acc->Integral()/4.0<<endl;
 
 
   TCanvas *c1 = new TCanvas("c1","c1",1600,1200);
@@ -222,91 +222,91 @@ void data_acc_substract(Int_t runNum){
   c1->Divide(3,3);
   c1->cd(1);
  
-  h_ctime->SetLineColor(kRed);
-  h_ctime->Draw("");
-  h_ctime->SetTitle(" ctime with pid cut");
-  h_ctime->SetLineWidth(2);
+  h_datactime->SetLineColor(kRed);
+  h_datactime->Draw("");
+  h_datactime->SetTitle(" ctime with pid cut");
+  h_datactime->SetLineWidth(2);
   c1->cd(2);
  
-  h_z->SetLineColor(kBlack);
-  h_z->Draw("e,p1");
-  h_z->SetTitle("zhadron with pid cut");
-  h_z->SetLineWidth(2);   
+  h_dataz->SetLineColor(kBlack);
+  h_dataz->Draw("e,p1");
+  h_dataz->SetTitle("zhadron with pid cut");
+  h_dataz->SetLineWidth(2);   
 
   c1->cd(3);
-  h_ctime_d->SetLineColor(kBlue);
-  h_ctime_d->Draw("");
-  h_ctime_d->SetTitle("ctime with pid within -2<ctime<2 (ns)");
-  h_ctime_d->SetLineWidth(2);	
+  h_datactime_d->SetLineColor(kBlue);
+  h_datactime_d->Draw("");
+  h_datactime_d->SetTitle("ctime with pid within -2<ctime<2 (ns)");
+  h_datactime_d->SetLineWidth(2);	
   
 
   c1->cd(4);
-  h_z_d->SetLineColor(kBlack);
-  h_z_d->Draw("e,p1");
-  h_z_d->SetTitle("zhadron with ctime cut");
-  h_z_d->SetLineWidth(2);   
+  h_dataz_d->SetLineColor(kBlack);
+  h_dataz_d->Draw("e,p1");
+  h_dataz_d->SetTitle("zhadron with ctime cut");
+  h_dataz_d->SetLineWidth(2);   
 
   c1->cd(5);
-  h_z_acc->SetLineColor(kBlack);
-  h_z_acc->Draw("e,p1");
-  h_z_acc->SetTitle("zhadron with accidental (4 times less counts will be the actual)");
-  h_z_acc->SetLineWidth(2);   
+  h_dataz_acc->SetLineColor(kBlack);
+  h_dataz_acc->Draw("e,p1");
+  h_dataz_acc->SetTitle("zhadron with accidental (6 times less counts will be the actual)");
+  h_dataz_acc->SetLineWidth(2);   
 
   c1->cd(6);
-  h_ctime->Draw();
-  h_ctime->SetLineColor(kBlue);
-  h_ctime_acc->SetLineColor(kRed);
-  h_ctime_acc->Draw("same");
-  h_ctime_acc->SetTitle("ctime with acc within 8 ns on either side of peak (ns)");
-  h_ctime_acc->SetLineWidth(2);	
+  h_datactime->Draw();
+  h_datactime->SetLineColor(kBlue);
+  h_datactime_acc->SetLineColor(kRed);
+  h_datactime_acc->Draw("same");
+  h_datactime_acc->SetTitle("ctime with acc within 8 ns on either side of peak (ns)");
+  h_datactime_acc->SetLineWidth(2);	
   
 
   c1->cd(7);
-  TH1D *h_z_d_copy = new TH1D("h_z_d_copy","h_z_d_copy",binz,loz,hiz);
-  TH1D *h_z_acc_copy = new TH1D("h_z_acc_copy","h_z_acc_copy",binz,loz,hiz);
+  TH1D *h_dataz_d_copy = new TH1D("h_dataz_d_copy","h_dataz_d_copy",binz,loz,hiz);
+  TH1D *h_dataz_acc_copy = new TH1D("h_dataz_acc_copy","h_dataz_acc_copy",binz,loz,hiz);
 
-  h_z_d_copy= (TH1D*)h_z_d->Clone("h_z_d_copy");//copy of h_z_d
-  h_z_acc_copy= (TH1D*)h_z_acc->Clone("h_z_acc_copy");//copy of h_z_acc
+  h_dataz_d_copy= (TH1D*)h_dataz_d->Clone("h_dataz_d_copy");//copy of h_dataz_d
+  h_dataz_acc_copy= (TH1D*)h_dataz_acc->Clone("h_dataz_acc_copy");//copy of h_dataz_acc
 
-  h_z_d_copy->Scale(1.0/4.0);
-  h_z_d->Draw("e,p1");
-  h_z_d_copy->Draw("same");
-  h_z_d_copy->SetLineColor(kRed);
+  h_dataz_d_copy->Scale(1.0/6.0);
+  h_dataz_d->Draw("e,p1");
+  h_dataz_d_copy->Draw("same");
+  h_dataz_d_copy->SetLineColor(kRed);
 
 
   c1->cd(8);
-  TH1D *h_ctime_d_copy = new TH1D("h_ctime_d_copy","h_ctime_d_copy",binct,loct,hict);
-  TH1D *h_ctime_acc_copy = new TH1D("h_ctime_acc_copy","h_ctime_acc_copy",binct,loct,hict);
+  TH1D *h_datactime_d_copy = new TH1D("h_datactime_d_copy","h_datactime_d_copy",binct,loct,hict);
+  TH1D *h_datactime_acc_copy = new TH1D("h_datactime_acc_copy","h_datactime_acc_copy",binct,loct,hict);
 
-  h_ctime_d_copy= (TH1D*)h_ctime_d->Clone("h_ctime_d_copy");//copy of h_ctime_d
-  h_ctime_acc_copy= (TH1D*)h_ctime_acc->Clone("h_ctime_acc_copy");//copy of h_ctime_acc
+  h_datactime_d_copy= (TH1D*)h_datactime_d->Clone("h_datactime_d_copy");//copy of h_datactime_d
+  h_datactime_acc_copy= (TH1D*)h_datactime_acc->Clone("h_datactime_acc_copy");//copy of h_datactime_acc
 
-  h_ctime_d_copy->Scale(1.0/4.0);
-  h_ctime_d->Draw("");
-  h_ctime_d_copy->Draw("same");
-  h_ctime_d_copy->SetLineColor(kRed);
+  h_datactime_d_copy->Scale(1.0/4.0);
+  h_datactime_d->Draw("");
+  h_datactime_d_copy->Draw("same");
+  h_datactime_d_copy->SetLineColor(kRed);
 
   c1->cd(9);
   
   ////======================do the accidental subtraction: corrected hist-accidental hist=========================================
 
-  TH1D *h_z_final = new TH1D("h_z_final","h_z_final",binz,loz,hiz);
-  h_z_final->Add( h_z_d,  h_z_d_copy, 1.0, -1.0);
-  h_z_final->Draw();
-  h_z_final->SetLineColor(3);
-  h_z_final->SetLineWidth(3);
+  TH1D *h_dataz_final = new TH1D("h_dataz_final","h_dataz_final",binz,loz,hiz);
+  h_dataz_final->Add( h_dataz_d,  h_dataz_d_copy, 1.0, -1.0);
+  h_dataz_final->Draw();
+  h_dataz_final->SetLineColor(3);
+  h_dataz_final->SetLineWidth(3);
 
-  cout<<"final counts = "<<h_z_final->Integral()<<endl;
+  cout<<"final counts = "<<h_dataz_final->Integral()<<endl;
 
   //ofstream printout(Form("txtfile/acc_sub_%d.txt",ios::app,runNum));
-  //printout<<"counts with other than ctime cut="<< h_ctime->Integral()<< " counts should ="<< h_ctime_should->Integral()<<" eff = "<<h_ctime_did->Integral()/h_ctime_should->Integral()<<endl;
+  //printout<<"counts with other than ctime cut="<< h_datactime->Integral()<< " counts should ="<< h_datactime_should->Integral()<<" eff = "<<h_datactime_did->Integral()/h_datactime_should->Integral()<<endl;
 
   ofstream printout(Form("txtfile/data_acc_%d.txt",runNum));// ios::app will keep on adding lines in the txt file.
-  printout<<runNum<< " " <<nentriesD<<" "<<  h_z->Integral() << " "<< h_z_d->Integral()<<"  "<< h_z_acc->Integral()<< " "<< h_z_d->Integral() - h_z_acc->Integral()/4.0<<" "<<h_z_final->Integral()<<endl;
-  h_z->Write();
-  h_z_d->Write();
-  h_z_acc->Write();
-  h_z_final->Write();
+  printout<<runNum<< " " <<nentriesD<<" "<<  h_dataz->Integral() << " "<< h_dataz_d->Integral()<<"  "<< h_dataz_acc->Integral()<< " "<< h_dataz_d->Integral() - h_dataz_acc->Integral()/6.0<<" "<<h_dataz_final->Integral()<<endl;
+  h_dataz->Write();
+  h_dataz_d->Write();
+  h_dataz_acc->Write();
+  h_dataz_final->Write();
   //ff->cd();
   //ff->Write();
 	       
@@ -315,6 +315,18 @@ void data_acc_substract(Int_t runNum){
   		
 
 }
+///////////////////////////////////////////////done///////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
 
 
