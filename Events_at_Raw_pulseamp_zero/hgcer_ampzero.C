00f4c88fe45b298fd7123f559687e7ed363f1eaf
hdb169 <hdb169@users.noreply.github.com>
2020-02-05 17:44:09 -0500

    Update and rename test_final.C to hgcer_ampzero.C

--- Events_at_Raw_pulseamp_zero/hgcer_ampzero.C
+++ Events_at_Raw_pulseamp_zero/hgcer_ampzero.C
@@ -16,8 +16,19 @@
 #include <TChain.h>
 #include <TTree.h>
 #include <TF1.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
+using namespace std;
 #define NRUNS 4
-void hgcer(){
+void hgcer_ampzero(){
+ofstream txtfile ("txtfile/hgcer_amp_zero.txt",ios::app);
+txtfile<<"======================================================================="<<endl;
+txtfile<<"Run Num"<<"\t"<<"det/pmt"<<"\t"<<"evts_amp >=0"<<"\t"<<"evts_amp = 0"<<"\t"<<"% of zero amp evts"<<"\t"<<endl;
+txtfile<<"======================================================================="<<endl;
 
 gROOT->SetBatch(kTRUE);
 
@@ -26,6 +37,8 @@ gROOT->SetBatch(kTRUE);
   for(Int_t i=0; i<NRUNS; i++){
     //TFile *f=new TFile(Form("/lustre/expphy/volatile/hallc/spring17/hdbhatt/group/ROOTfiles/coin_replay_production_%d_20000.root", runs[i]),"read");
      TFile *f=new TFile(Form("/lustre/expphy/volatile/hallc/spring17/hdbhatt/group/ROOTfiles/amp_zero/coin_replay_production_%d_-1.root", runs[i]),"read");
+     TFile *f1 = new TFile("root/hgcer_hist.root","RECREATE");
+
 
     if ((!f) || (f->IsZombie())){
       cout << "No file found or Zombie file" << endl;
@@ -36,24 +49,24 @@ gROOT->SetBatch(kTRUE);
     Double_t nbins =1000;
     Double_t lo = -1000;
     Double_t hi = 15000;
-    ofstream txtfile1 ("txtfile/hgcer_amp_zero_all_pmts.txt",ios::app);
-    ofstream txtfile2 ("txtfile/hgcer_amp_zero_individual_pmts.txt",ios::app);
+
+
 
     ///////////for first Canvas which will have info for the whole detector///////////////
-    TH1F * hgcer_allpmt_allamp = new TH1F ("hgcer_allpmt_allamp","hgcer_allpmt_allamp",nbins, lo,hi);
-    TH1F * hgcer_allpmt_noamp  = new TH1F ("hgcer_allpmt_noamp","hgcer_allpmt_noamp",nbins, lo,hi);
+    TH1F * hgcer_allpmt_allamp = new TH1F ("hgcer_allpmt_allamp","hgcer_allpmt_allamp",nbins, lo,hi);//hist for whole detector: amp >=0
+    TH1F * hgcer_allpmt_noamp  = new TH1F ("hgcer_allpmt_noamp","hgcer_allpmt_noamp",nbins, lo,hi);//hist for whole detector: amp =0
 
     tt->Draw("P.hgcer.adcPedRaw>>hgcer_allpmt_allamp","P.hgcer.adcPulseAmpRaw>=0");
     tt->Draw("P.hgcer.adcPedRaw>>hgcer_allpmt_noamp","P.hgcer.adcPulseAmpRaw==0");
 
     ///////////for Second Canvas, which will have info for all the PMTs in the detector///////////////
-    TH1F *h1[4];
-    TH1F *h2[4];
-    Double_t evts_ind_all[4];
-    Double_t evts_ind_no[4]; 
+    TH1F *h1[4];//hist for 4 pmts in hgcer (for amplitude >=0)
+    TH1F *h2[4];//hist for 4 pmts in hgcer (for amplitude ==0 0)
+    Double_t evts_ind_all[4];// prints events for 4 pmts  amp >=0
+    Double_t evts_ind_no[4]; //prints events for 4 pmts amp ==0
 
     for (int icounter=0; icounter<4; icounter++){
-   
+      //P.hgcer.adcCounter=1=> pmt1 and so on
       
 
       h1[icounter] = new TH1F(Form("hgcer_pmt%d_amp",icounter+1),"",nbins, lo, hi);
@@ -72,16 +85,19 @@ gROOT->SetBatch(kTRUE);
     hgcer_allpmt_allamp->Draw("");
     hgcer_allpmt_allamp->SetLineColor(kBlue);
     hgcer_allpmt_noamp->Draw("sames");
-    hgcer_allpmt_noamp->SetLineColor(30);
+    hgcer_allpmt_noamp->SetLineColor(kRed);
     hgcer_allpmt_allamp->GetXaxis()->SetTitle("P.hgcer.adcPedRaw");
     hgcer_allpmt_allamp->SetTitle(Form("hgcer_all_pmts_%d",runs[i]));
     hgcer_allpmt_allamp->GetXaxis()->CenterTitle();
     hgcer_allpmt_allamp->GetXaxis()->SetTitleOffset(1.56);
     hgcer_allpmt_allamp->GetXaxis()->SetAxisColor(17);
     hgcer_allpmt_allamp->GetYaxis()->SetAxisColor(17);
+    hgcer_allpmt_allamp->Write();
+    hgcer_allpmt_noamp->Write();
+    Double_t evts_tot_all = hgcer_allpmt_allamp->GetEntries();// for whole detector with amp >=0
+    Double_t evts_tot_no  = hgcer_allpmt_noamp->GetEntries();// for whole detector with amp ==0
 
-    Double_t evts_tot_all = hgcer_allpmt_allamp->GetEntries();
-    Double_t evts_tot_no  = hgcer_allpmt_noamp->GetEntries();
+    txtfile << runs[i]<<"\t"<<std::setprecision(2)<<fixed<<"hgcer"<<"\t"<<evts_tot_all<<"\t"<<evts_tot_no<<"\t"<<evts_tot_no *100./evts_tot_all<< " % "<<endl;
 
     TCanvas * c1 = new TCanvas("c1","hgcer_test",1600,1200);
     c1->Divide(2,2);
@@ -92,32 +108,26 @@ gROOT->SetBatch(kTRUE);
 	h1[j]->Draw();
 	h1[j]->GetXaxis()->SetTitle("P.hgcer.adcPedRaw");
 	h1[j]->SetLineColor(kBlue);
-	h2[j]->Draw("sames");
-	h2[j]->SetLineColor(30);
 	h1[j]->SetTitle(Form("hgcer_pmt_%d_%d",j+1,runs[i]));
 	h1[j]->GetXaxis()->SetAxisColor(17);
 	h1[j]->GetYaxis()->SetAxisColor(17);
+	h2[j]->Draw("sames");
+	h2[j]->SetLineColor(kRed);
 
-        evts_ind_all[j] = h1[j]->GetEntries();
-	evts_ind_no[j] = h2[j]->GetEntries();
+        evts_ind_all[j] = h1[j]->GetEntries();//for individual pmts with amp >=0
+	evts_ind_no[j]  = h2[j]->GetEntries();// for individula pmts with amp ==0
+       
+	h1[j]->Write();
+	h2[j]->Write();
 
-
-	cout<<setprecision(2)<<fixed<<"\t"<<runs[i]<<"\t"<< Form("pmt_%d",j+1)<<"\t"<<evts_ind_all[j]<<"\t"<< evts_ind_no[j]<<"\t"<< evts_ind_no[j]*100./evts_ind_all[j]<<" % "<<"\t"<< endl;
-	cout<<"\t"<<endl;
-        txtfile2<<setprecision(2)<<fixed<<runs[i]<<"\t"<<Form("pmt_%d",j+1)<<"\t"<<evts_ind_all[j]<<"\t"<< evts_ind_no[j]<<"\t"<<evts_ind_no[j]*100./evts_ind_all[j]<<" % "<<"\t"<<endl;
-	txtfile2<<"\t"<<endl;
+        txtfile<<runs[i]<<"\t"<<std::setprecision(2)<<fixed<<Form("pmt_%d",j+1)<<"\t"<<evts_ind_all[j]<<"\t"<<evts_ind_no[j]<<"\t"<<evts_ind_no[j]*100./evts_ind_all[j]<<" % "<<"\t"<<endl;
       }
-    cout<<setprecision(2)<<fixed<<runs[i]<<"\t"<<" hgcer_allpmts "<<evts_tot_all<<"\t"<<evts_tot_no<<"\t"<<evts_tot_no *100./evts_tot_all<< " % "<<"\t"<< endl;
-    cout<<"\t"<<endl;
 
-    txtfile1<<setprecision(2)<<fixed<<runs[i]<<"\t"<<" hgcer_allpmts "<<evts_tot_all<<"\t"<<evts_tot_no<<"\t"<<evts_tot_no *100./evts_tot_all<< " % "<<endl;
-    txtfile1<<"\t"<<endl;
+    txtfile<<"======================"<<Form("Run Num %d",runs[i])<<" done "<<"========================"<<endl;
+    txtfile<<"                                                                                        "<<endl;
 
-
-
-
-    cc->SaveAs(Form("pdf/hgcer_allpmts_%d.pdf",runs[i]));
-    c1->SaveAs(Form("pdf/hgcer_individual_pmts_%d.pdf",runs[i]));
+    cc->SaveAs(Form("pdf/hgcer_whole_%d.pdf",runs[i]));//just one plot
+    c1->SaveAs(Form("pdf/hgcer_pmts_%d.pdf",runs[i]));//for all pmts
     cc->~TCanvas();
     c1->~TCanvas();
 
