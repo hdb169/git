b0e768d714dc23d1af6fe163bf25891a24dc2303
hdb169 <hdb169@users.noreply.github.com>
2020-04-10 20:21:34 -0400

    Create binz.h

---
+++ rftime/Q2_5p5_3p2/3p2_acc_dummy/binz.h
@@ -0,0 +1,50 @@
+Int_t binz =8;
+Double_t loz =0.54;
+Double_t hiz= 0.78;
+
+
+  
+Int_t binct =100;
+Int_t loct =30;
+Int_t hict=60;
+
+//fixed
+Int_t binaccct =100;
+Int_t loaccct =20;
+Int_t hiaccct=70;
+
+Int_t binmult =100;
+Int_t lomult =-2;
+Int_t himult=10;
+
+Int_t bintdctime =100;
+Int_t lotdctime =-1000;
+Int_t hitdctime=1000;
+
+Int_t bindiff =100;
+Int_t lodiff =-1000;
+Int_t hidiff =3000;
+
+Int_t binhitstime =100;
+Int_t lohitstime = -1000;
+Int_t hihitstime=1000;
+
+Int_t bincal=100;
+Int_t local = 0;
+Double_t hical = 1.2;
+
+Int_t bincalepr=100;
+Double_t localepr = 0;
+Double_t hicalepr = 0.5;
+
+Double_t pionMass = 0.1395701835;
+
+Int_t binhgc =100;
+Double_t binlohgc = 0.0001;
+Int_t binhihgc = 30;
+
+Int_t binrf =100;
+Double_t binrflo = -0.5;
+Double_t binrfhi = 4.5;
+
+  
