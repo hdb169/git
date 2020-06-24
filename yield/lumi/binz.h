a2c4f11b338754faaa5dd6cfc341fb8e85f9a4a7
hdb169 <hdb169@users.noreply.github.com>
2020-06-24 14:54:22 -0400

    Add files via upload

---
+++ yield/lumi/binz.h
@@ -0,0 +1,53 @@
+Int_t binz =10;
+Double_t loz= 0.5;
+Double_t hiz= 0.7;
+
+
+  
+Int_t binct =200;
+Int_t loct =20;
+Int_t hict=70;
+
+//fixed
+Int_t binaccct =100;
+Int_t loaccct =20;
+Int_t hiaccct=70;
+
+Int_t binmult =100;
+Int_t lomult =-2;
+Int_t himult=10;
+
+Int_t bintdctime =100;
+Int_t lotdctime =-1000;
+Int_t hitdctime=1000;
+
+Int_t bindiff =100;
+Int_t lodiff =-1000;
+Int_t hidiff =3000;
+
+Int_t binhitstime =100;
+Int_t lohitstime = -1000;
+Int_t hihitstime=1000;
+
+Int_t bincal=100;
+Int_t local = 0;
+Double_t hical = 1.2;
+
+Int_t bincalepr=100;
+Double_t localepr = 0;
+Double_t hicalepr = 0.5;
+
+Double_t pionMass = 0.1395701835;
+
+Int_t binhgc =100;
+Double_t binlohgc = 0.0001;
+Int_t binhihgc = 30;
+
+Int_t binrf =100;
+Double_t binrflo = -0.5;
+Double_t binrfhi = 4.5;
+
+  
+Int_t binshwr=100;
+Double_t loshwr = 0;
+Double_t hishwr = 1.5;
---
+++ yield/lumi/header.h
@@ -0,0 +1,25 @@
+#define ROOT_header
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TVectorD.h"
+#include "TMatrixD.h"
+#include "TDecompLU.h"
+#include "TMath.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "TROOT.h"
+#include "TFile.h"
+#include "TTree.h"
+#include <time.h>
+#include <vector>
+#include <string>
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TSpectrum.h>
+#include <TPolyMarker.h>
+#include <TMath.h>
+#include <TSystem.h>
+
