fd12647fc07ced2fab2d4f9404bd8ad7b4184d03
hdb169 <hdb169@users.noreply.github.com>
2021-05-31 11:36:55 -0400

    Create offsetrf.h

---
+++ rftime/RFMay30/offsetrf.h
@@ -0,0 +1,91 @@
+//kin1-2
+if(runs[r]>=6020 && runs[r]<=6022){offsetrf = 397.1;}//kin1-2
+if(runs[r]>=6026 && runs[r]<=6029){offsetrf = 397.05;}
+if(runs[r]>=6030 && runs[r]<=6043){offsetrf = 397.1;}
+if(runs[r]>=6044 && runs[r]<=6046){offsetrf = 397.05;}
+if(runs[r]>=6049 && runs[r]<=6058){offsetrf = 399.0;}
+if(runs[r]>=6067 && runs[r]<=6068){offsetrf = 399.1;}
+if(runs[r]>=6070 && runs[r]<=6073){offsetrf = 397.1;}
+if(runs[r]>=6080 && runs[r]<=6090){offsetrf = 399.1;}
+if(runs[r]==6091) {offsetrf = 399.05;}
+if(runs[r]>=6093 && runs[r]<=6098){offsetrf = 399.0;}
+
+//3_4
+if(runs[r]>=6537 && runs[r]<= 6552){offsetrf = 399.05;}
+//5_6
+if(runs[r]>=6145 && runs[r]<=6172){offsetrf =401.05;}//all good
+if(runs[r]>=6109 && runs[r]<= 6125){offsetrf =399.05;}//all good
+if(runs[r]>=6126 && runs[r]<= 6128){offsetrf =399.0;}//all good
+if(runs[r]==6129){offsetrf= 398.95;}//all good 
+if(runs[r]>=6130 && runs[r]<= 6138){offsetrf =396.95;}//all good    
+if(runs[r]>=6179 && runs[r]<=6187){offsetrf =396.95;}//all good
+//7-8
+if(runs[r]>=6486 && runs[r]<=6487){offsetrf = 399.05;}
+if(runs[r]==6488){offsetrf = 399.0;}
+if(runs[r]==6489){offsetrf = 399.05;}   
+if(runs[r]>=6490 && runs[r]<=6491){offsetrf = 399.0;}
+if(runs[r]>=6492 && runs[r]<=6494){offsetrf = 398.95;}
+if(runs[r]>=6495 && runs[r]<=6496){offsetrf = 398.9;}
+if(runs[r]==6497){offsetrf = 394.9;}
+if(runs[r]==6498){offsetrf = 394.8;}
+if(runs[r]==6499){offsetrf = 394.87;}
+if(runs[r]>=6500 && runs[r]<=6507){offsetrf = 399.05;}
+if(runs[r]>=6509 && runs[r]<=6510){offsetrf = 399.0;}    
+if(runs[r]==6512){offsetrf = 396.95;}
+if(runs[r]>=6513 && runs[r]<=6517){offsetrf = 396.9;}
+//9-10
+if (runs[r]>=6194 && runs[r]<= 6195){offsetrf = 397.08;}
+if (runs[r]>=6196 && runs[r]<= 6199){offsetrf = 397.05;}
+if (runs[r]>=6205 && runs[r]<= 6212){offsetrf = 397.00;}
+if (runs[r]>=6213 && runs[r]<= 6217){offsetrf = 396.95;}
+if (runs[r]>=6219 && runs[r]<= 6220){offsetrf = 398.95;}
+if (runs[r]>=6242 && runs[r]<= 6250){offsetrf = 399.05;}
+if (runs[r]>=6251 && runs[r]<= 6252){offsetrf = 399.00;}
+if (runs[r]>=6261 && runs[r]<= 6262){offsetrf = 399.00;}
+if (runs[r]>=6263 && runs[r]<= 6267){offsetrf = 399.05;}
+if (runs[r]>=6270 && runs[r]<= 6276){offsetrf = 396.95;}
+
+//11-12
+if(runs[r]>=6419 && runs[r]<= 6479){offsetrf = offsetrf = 399.05;}
+//13_14
+
+if(runs[r]>=6288 && runs[r]<=6293){offsetrf = 397.05;}    
+if(runs[r]>=6294 && runs[r]<=6303){offsetrf = 397.00;}
+if(runs[r]>=6304 && runs[r]<=6305){offsetrf = 396.97;}
+if(runs[r]>=6306 && runs[r]<=6308){offsetrf = 397.00;}    
+if(runs[r]>=6309 && runs[r]<=6311){offsetrf = 397.05;}
+if(runs[r]>=6312 && runs[r]<=6321){offsetrf = 399.00;}    
+if(runs[r]>=6322 && runs[r]<=6323){offsetrf = 399.05;}   
+if(runs[r]>=6324 && runs[r]<=6329){offsetrf = 399.00;}
+if(runs[r]>=6339 && runs[r]<=6348){offsetrf = 398.94;}
+if(runs[r]>=6349 && runs[r]<=6351){offsetrf = 398.95;}
+if(runs[r]>=6352 && runs[r]<=6354){offsetrf = 398.92;}
+
+//15_16
+if(runs[r]>=6359 && runs[r]<=6372){offsetrf = 399.00;}
+if(runs[r]==6373 && runs[r]==6374){offsetrf = 398.90;}
+if(runs[r]>=6375 && runs[r]<=6377){offsetrf = 399.00;}
+if(runs[r]>=6378 && runs[r]<=6383){offsetrf = 398.90;}
+if(runs[r]>=6386 && runs[r]<=6398){offsetrf = 399.05;}
+if(runs[r]>=6399 && runs[r]<=6404){offsetrf = 399.00;}
+if(runs[r]>=6405 && runs[r]<=6417){offsetrf = 398.90;}
+//17-18
+if(runs[r]>=6518 && runs[r]<=6536){offsetrf = 399.07;}
+
+
+//////////spring/////////
+if(runs[r]>=7593 && runs[r]<=7659){offsetrf = 400.00;}//kin33-34
+if(runs[r]>=7660 && runs[r]<=7670){offsetrf = 399.95;}//kin33-34
+
+//if(runs[r]>=7593 && runs[r]<=7670){offsetrf = 400.00;}//kin33-34 Jan 27
+
+if(runs[r]>=7671 && runs[r]<=7698){offsetrf = 400.10;}
+
+if(runs[r]>=7699 && runs[r]<=7699){offsetrf = 400.08;}//jan 25, 2020
+if(runs[r]>=7700 && runs[r]<=7722){offsetrf = 398.1;}//.08 0r 0.1
+if(runs[r]>=7724 && runs[r]<=7725){offsetrf = 398.05;}
+if(runs[r]>=7727 && runs[r]<=7736){offsetrf = 498.25;}
+if(runs[r]>=7737 && runs[r]<=7792){offsetrf = 400.00;}//400.05 is better
+if(runs[r]>=7793 && runs[r]<=7807){offsetrf = 400.05;}
+if(runs[r]>=7808 && runs[r]<=7820){offsetrf = 400.05;}
+
